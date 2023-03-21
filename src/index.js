56ad87557509f9cc673b439fd363d71c206cbdc8
RostyslavMykhats <mirosrik@gmail.com>
2023-03-21 14:36:27 +0200

    done

--- src/index.js
+++ src/index.js
@@ -5,53 +5,19 @@ import "./index.scss";
 // Import modules
 // import { calcSumNumbers, calcSubtractNumbers } from "./scripts/modules/calc";
 // import { Elements } from "./scripts/modules/elements";
-
+import {sliderVisitors} from "./scripts/modules/visitorsslide";
+import {animalsSlide } from "./scripts/modules/animalsSlide";
+import { tabsAnimals } from "./scripts/modules/tabsAnimals";
+import { newsSlide } from "./scripts/modules/newsSlide";
 // Import methods
 // import { includeHTML } from "./scripts/methods/_includeComponents";
 
 // Bisiness logic
 document.addEventListener('DOMContentLoaded', () => {
-
-    
-
-    const swiper = new Swiper('.swiper', {
-        // Optional parameters
-        slidesPerView: 1,
-        slidesPerGroup: 1,
-        spaceBetween: 60,
-        loop: true,
-
-        // If we need pagination
-        pagination: {
-            el: '.swiper-pagination',
-        },
-
-        // Navigation arrows
-        navigation: {
-            nextEl: '.next-slide',
-            prevEl: '.prev-slide',
-        },
-
-        // And if we need scrollbar
-        scrollbar: {
-            el: '.swiper-scrollbar',
-        },
-
-        breakpoints: {
-            // when window width is >= 320px
-            320: {
-                slidesPerView: 1,
-                spaceBetween: 60,
-                slidesPerGroup: 1,
-            },
-
-            1000: {
-                slidesPerView: 2,
-                spaceBetween: 60,
-                slidesPerGroup: 2,
-            },
-        }
-    });
+    sliderVisitors();
+    animalsSlide();
+    tabsAnimals();
+    newsSlide();
 });
 
 
